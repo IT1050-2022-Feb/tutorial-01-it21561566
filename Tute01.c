3d5746ccc4b460869342185c51f1040280091c8c
it21561566 <99281273+it21561566@users.noreply.github.com>
2022-02-09 00:18:49 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,8 +5,18 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello World");
-  
-  return 0;
+  int sum , mark ;
+  float avg;
+     for( int i=1 ; i<=2 ; i++){
+      printf("Subject No.%d Marks :" , i);
+      scanf("%d" , &mark);
+
+      sum = sum + mark;
+      avg = (float)sum / i;
+      }
+   printf("\nTotal of the 2 Marks : %d\n" , sum);
+   printf("Average of the 2 Marks : %.2f\n" , avg);
+
+   return 0;
 }
 
