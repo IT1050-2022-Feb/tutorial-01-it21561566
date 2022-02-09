64b77743a0bd76e5d165e0eaf2d7919aec4fe53c
it21561566 <99281273+it21561566@users.noreply.github.com>
2022-02-09 10:10:19 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -25,14 +25,11 @@ int main()
 }
 
 int minimum(int no1,int no2){
-
     return (no1 > no2 ) ? no1 : no2;   
 }
 
 int maximum(int no1,int no2){
-
-    return (no1 > no2 ) ? no2 : no1;
-    
+   return (no1 > no2 ) ? no2 : no1; 
 }
 
 int multiply(int no1,int no2){
